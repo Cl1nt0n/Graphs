e3eaa1d7d634caaefe59469fb009cf092c4a8125
Igor <76621236+Cl1nt0n@users.noreply.github.com>
2023-11-06 23:23:47 +0300

    Add files via upload

--- GraphLaba/GraphLaba/GraphLaba.cpp
+++ GraphLaba/GraphLaba/GraphLaba.cpp
@@ -1,21 +1,106 @@
-﻿// GraphLaba.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include "GraphLaba.h"
+#define size 6
 
 using namespace std;
+
+void DijkstraAlgorithm(int* passed_peaks, int* min_weight, int  matrix[][size], int result_array[][size]);
+
 int main()
 {
-	
+	//Подключаем русский язык
+	setlocale(LC_ALL, "ru");
+
+	//Матрица смежности
+	int matrix[size][size] =
+	{
+		{0,   7,  9, -1, -1, 14},
+		{7,   0, 10, 15, -1, -1},
+		{9,  10,  0, 11, -1,  2},
+		{-1, 15, 11,  0,  6, -1},
+		{-1, -1, -1,  6,  0,  9},
+		{14, -1,  2, -1,  9,  0}
+	};
+
+	int result_array[size][size];
+	//Массив минимальных расстояний до вершин
+	int* min_weight = new int[size];
+	//Массив пройденных вершин
+	int* passed_peaks = new int[size];
+
+	cout << "Матрица смежности графа:" << endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			cout << matrix[i][j] << '\t';
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	DijkstraAlgorithm(passed_peaks, min_weight, matrix, result_array);
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Кратчайшие расстояния от " << i + 1 << " вершины" << endl;
+		for (int j = 0; j < size; j++)
+		{
+			cout << j + 1 << ':' << result_array[i][j] << endl;
+		}
+	}
+
+	delete[] min_weight;
+	delete[] passed_peaks;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void DijkstraAlgorithm(int* passed_peaks, int* min_weight, int  matrix[][size], int result_array[][size])
+{
+	int min_index, min;
+	for (int j = 0; j < size; j++)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			*(min_weight + i) = INT_MAX;
+			*(passed_peaks + i) = 1;
+		}
+		*(min_weight + j) = 0;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+		do
+		{
+			min_index = INT_MAX;
+			min = INT_MAX;//минимальный вес не пройденной вершины
+			for (int i = 0; i < size; i++)
+			{ // Если вершину ещё не обошли и вес меньше min
+				if ((passed_peaks[i] == 1) && (min_weight[i] < min))
+				{
+					min = min_weight[i];
+					min_index = i;
+				}
+			}
+
+			// Добавляем найденный минимальный вес
+			// к текущему весу вершины
+			// и сравниваем с текущим минимальным весом вершины
+			if (min_index != INT_MAX)
+			{
+				for (int i = 0; i < size; i++)
+				{
+					if (matrix[min_index][i] > 0)
+					{
+						int temp = min + matrix[min_index][i];
+						if (temp < min_weight[i])
+						{
+							min_weight[i] = temp;
+						}
+					}
+				}
+				passed_peaks[min_index] = 0;
+			}
+		} while (min_index < INT_MAX);
+
+		for (int i = 0; i < size; i++)
+			result_array[j][i] = min_weight[i];
+	}
+}
---
+++ GraphLaba/GraphLaba/GraphLaba.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void DijkstraAlgorithm(int& min_index, int& min, int* passed_peaks, int* min_weight, int  matrix[6][6]);
